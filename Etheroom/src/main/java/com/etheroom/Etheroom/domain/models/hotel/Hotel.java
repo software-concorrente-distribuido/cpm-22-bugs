44bca96c3a9ba14f9eb4fbb9c12667a4460b224f
Robert-Martins <robertcantares@gmail.com>
2024-06-07 17:26:34 -0300

    feat/define-hotel-entity-and-ddl

--- Etheroom/src/main/java/com/etheroom/Etheroom/domain/models/hotel/Hotel.java
+++ Etheroom/src/main/java/com/etheroom/Etheroom/domain/models/hotel/Hotel.java
@@ -1,11 +1,15 @@
 package com.etheroom.Etheroom.domain.models.hotel;
 
+import com.etheroom.Etheroom.domain.models.address.Address;
+import com.etheroom.Etheroom.domain.models.contact.Contact;
+import com.etheroom.Etheroom.domain.models.user.User;
 import com.etheroom.Etheroom.infrastructure.base.BaseEntity;
 import com.etheroom.Etheroom.presentation.dtos.hotel.HotelDto;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Optional;
 
 @Getter
 @Setter
@@ -13,9 +17,48 @@ import lombok.Setter;
 @Entity
 public class Hotel extends BaseEntity {
 
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "contact_id", referencedColumnName = "id")
+    private Contact contact;
+
     @Override
     public HotelDto mapEntityToDto() {
-        return null;
+        HotelDto hotelDto = new HotelDto();
+        hotelDto.setId(this.getId());
+        hotelDto.setName(this.getName());
+        hotelDto.setDescription(this.getDescription());
+        hotelDto.setUser(
+                Optional.ofNullable(this.getUser())
+                        .map(User::mapEntityToDto)
+                        .orElse(null)
+        );
+        hotelDto.setAddress(
+                Optional.ofNullable(this.getAddress())
+                        .map(Address::mapEntityToDto)
+                        .orElse(null)
+        );
+        hotelDto.setContact(
+                Optional.ofNullable(this.getContact())
+                        .map(Contact::mapEntityToDto)
+                        .orElse(null)
+        );
+        hotelDto.setUpdatedAt(this.getUpdatedAt());
+        hotelDto.setCreatedAt(this.getCreatedAt());
+        return hotelDto;
     }
 
 }
--- Etheroom/src/main/java/com/etheroom/Etheroom/presentation/dtos/hotel/HotelDto.java
+++ Etheroom/src/main/java/com/etheroom/Etheroom/presentation/dtos/hotel/HotelDto.java
@@ -2,16 +2,48 @@ package com.etheroom.Etheroom.presentation.dtos.hotel;
 
 import com.etheroom.Etheroom.domain.models.hotel.Hotel;
 import com.etheroom.Etheroom.infrastructure.base.BaseEntityDto;
+import com.etheroom.Etheroom.presentation.dtos.address.AddressDto;
+import com.etheroom.Etheroom.presentation.dtos.contact.ContactDto;
+import com.etheroom.Etheroom.presentation.dtos.user.UserDto;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Optional;
 
 @Getter
 @Setter
 public class HotelDto extends BaseEntityDto<Hotel> {
 
+    private String name;
+    private String description;
+    private UserDto user;
+    private AddressDto address;
+    private ContactDto contact;
+
     @Override
     public Hotel mapDtoToEntity() {
-        return null;
+        Hotel hotel = new Hotel();
+        hotel.setId(this.getId());
+        hotel.setName(this.getName());
+        hotel.setDescription(this.getDescription());
+        hotel.setUser(
+                Optional.ofNullable(this.getUser())
+                        .map(UserDto::mapDtoToEntity)
+                        .orElse(null)
+        );
+        hotel.setAddress(
+                Optional.ofNullable(this.getAddress())
+                        .map(AddressDto::mapDtoToEntity)
+                        .orElse(null)
+        );
+        hotel.setContact(
+                Optional.ofNullable(this.getContact())
+                        .map(ContactDto::mapDtoToEntity)
+                        .orElse(null)
+        );
+        hotel.setUpdatedAt(this.getUpdatedAt());
+        hotel.setCreatedAt(this.getCreatedAt());
+        return hotel;
     }
 
 }
