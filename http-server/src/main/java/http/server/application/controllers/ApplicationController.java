86c9876e0dc59a2f87bf313df82accfb976fd19d
Robert-Martins <robertcantares@gmail.com>
2024-06-21 16:47:43 -0300

    feat/implement-application-controller-with-custom-http-handlers

--- http-server/src/main/java/http/server/application/controllers/ApplicationController.java
+++ http-server/src/main/java/http/server/application/controllers/ApplicationController.java
@@ -1,5 +1,6 @@
 package http.server.application.controllers;
 
+import http.server.application.dtos.ItemResponse;
 import http.server.application.services.IApplicationService;
 import http.server.domain.services.ApplicationService;
 import http.server.infrastructure.utils.HttpUtils;
@@ -11,18 +12,22 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 public class ApplicationController extends HttpServlet {
 
-    private IApplicationService applicationService = ApplicationService.getInstance();
+    private final IApplicationService applicationService = ApplicationService.getInstance();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         this.configureCors(resp);
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(req.getInputStream(), "UTF-8"))) {
-            this.applicationService.create(JsonUtils.fromJson(reader));
-            HttpUtils.setCreatedResponse(resp, "Candidato criado com sucesso");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(req.getInputStream(), StandardCharsets.UTF_8))) {
+            ItemResponse itemResponse = this.applicationService.create(JsonUtils.fromJson(reader));
+            HttpUtils.setCreatedResponse(
+                    resp,
+                    itemResponse
+            );
         } catch (IOException e) {
             e.printStackTrace();
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
@@ -36,7 +41,7 @@ public class ApplicationController extends HttpServlet {
         resp.setCharacterEncoding("UTF-8");
         Optional.ofNullable(req.getParameter("id"))
                 .ifPresentOrElse(
-                        id -> this.findById(Integer.parseInt(id), resp),
+                        id -> this.findById(id, resp),
                         () -> this.findAll(resp)
                 );
     }
@@ -44,9 +49,12 @@ public class ApplicationController extends HttpServlet {
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         this.configureCors(resp);
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(req.getInputStream(), "UTF-8"))) {
-            this.applicationService.update(JsonUtils.fromJson(reader));
-            HttpUtils.setCreatedResponse(resp, "Candidato atualizado com sucesso");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(req.getInputStream(), StandardCharsets.UTF_8))) {
+            this.applicationService.update(
+                    req.getParameter("id"),
+                    JsonUtils.fromJson(reader)
+            );
+            HttpUtils.setNoContentResponse(resp, "Item atualizado com sucesso");
         } catch (IOException e) {
             e.printStackTrace();
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
@@ -60,20 +68,39 @@ public class ApplicationController extends HttpServlet {
         this.configureCors(resp);
         Optional.ofNullable(req.getParameter("id"))
                 .ifPresent(
-                        id -> this.deleteById(Integer.parseInt(id), resp)
+                        id -> this.deleteById(id, resp)
                 );
     }
 
     @Override
-    protected void doOptions(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
+    protected void doOptions(HttpServletRequest request, HttpServletResponse response) {
         this.configureCors(response);
         response.setStatus(HttpServletResponse.SC_OK);
     }
 
-    private void findById(Integer id, HttpServletResponse resp) {
+    @Override
+    protected void doHead(HttpServletRequest request, HttpServletResponse response) {
+        this.configureCors(response);
         try {
-            HttpUtils.setOkResponse(resp, this.candidateService.findById(id));
+            HttpUtils.setMethodNotAllowedResponse(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doTrace(HttpServletRequest request, HttpServletResponse response) {
+        this.configureCors(response);
+        try {
+            HttpUtils.setMethodNotAllowedResponse(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void findById(String id, HttpServletResponse resp) {
+        try {
+            HttpUtils.setOkResponse(resp, this.applicationService.findById(id));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -81,16 +108,16 @@ public class ApplicationController extends HttpServlet {
 
     private void findAll(HttpServletResponse resp) {
         try {
-            HttpUtils.setOkResponse(resp, this.candidateService.findAll());
+            HttpUtils.setOkResponse(resp, this.applicationService.findAll());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void deleteById(Integer id, HttpServletResponse resp) {
+    private void deleteById(String id, HttpServletResponse resp) {
         try {
-            this.candidateService.deleteById(id);
-            HttpUtils.setOkResponse(resp, "Candidato deletado com sucesso");
+            this.applicationService.deleteById(id);
+            HttpUtils.setNoContentResponse(resp, "Item deletado com sucesso");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -98,7 +125,7 @@ public class ApplicationController extends HttpServlet {
 
     private void configureCors(HttpServletResponse response) {
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH, HEAD, TRACE");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "true");
--- http-server/src/main/java/http/server/infrastructure/utils/HttpUtils.java
+++ http-server/src/main/java/http/server/infrastructure/utils/HttpUtils.java
@@ -1,27 +1,67 @@
 package http.server.infrastructure.utils;
 
+import http.server.application.dtos.ApplicationResponse;
+
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Writer;
 
 public class HttpUtils {
 
-    private static final Integer CREATED_STATUS = 201;
-
-    private static final Integer OK_STATUS = 200;
-
     public static <T> void setCreatedResponse(HttpServletResponse response, T responseBody) throws IOException {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        response.setStatus(CREATED_STATUS);
-        setResponseBody(response, responseBody);
+        response.setStatus(HttpServletResponse.SC_CREATED);
+        setResponseBody(
+                response,
+                ApplicationResponse.fromSuccess(
+                        HttpServletResponse.SC_CREATED,
+                        "Requisição realizada com sucesso",
+                        responseBody
+                )
+        );
     }
 
     public static <T> void setOkResponse(HttpServletResponse response, T responseBody) throws IOException {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        response.setStatus(OK_STATUS);
-        setResponseBody(response, responseBody);
+        response.setStatus(HttpServletResponse.SC_OK);
+        setResponseBody(
+                response,
+                ApplicationResponse.fromSuccess(
+                        HttpServletResponse.SC_OK,
+                        "Requisição realizada com sucesso",
+                        responseBody
+                )
+        );
+    }
+
+    public static void setNoContentResponse(HttpServletResponse response, String message) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        setResponseBody(
+                response,
+                ApplicationResponse.fromSuccess(
+                        HttpServletResponse.SC_OK,
+                        message,
+                        null
+                )
+        );
+    }
+
+    public static void setMethodNotAllowedResponse(HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+        setResponseBody(
+                response,
+                ApplicationResponse.fromError(
+                        HttpServletResponse.SC_METHOD_NOT_ALLOWED,
+                        "Método não permitido",
+                        null
+                )
+        );
     }
 
     private static <T> void setResponseBody(HttpServletResponse response, T responseBody) throws IOException {
