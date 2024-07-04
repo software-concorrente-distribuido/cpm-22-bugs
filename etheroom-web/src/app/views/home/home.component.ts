ab03dc6a66f72931c829b23ef3764367a3ad63b6
WendelFl <wendelfl.dev@gmail.com>
2024-07-04 20:04:41 -0300

    :construction: feat: home in progress

--- etheroom-web/src/app/views/home/home.component.ts
+++ etheroom-web/src/app/views/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HotelInfoComponent } from '../../shared/components/hotel-info/hotel-info.component';
 import {MatTabsModule} from '@angular/material/tabs';
-import { EtherInputFieldComponent } from '../../shared/components/ether-input-field/ether-input-field.component';
 import { EtherPageComponent } from '../../shared/components/containers/ether-page/ether-page.component';
 import { SharedModule } from '../../shared/shared.module';
 import { EtherHomeSectionTitleComponent } from '../../shared/components/ether-home-section-title/ether-home-section-title.component';
@@ -25,7 +24,6 @@ export enum HotelsTypes {
     MatTabsModule,
     HotelInfoComponent,
     EtherPageComponent,
-    EtherInputFieldComponent,
     EtherHomeSectionTitleComponent
   ],
   templateUrl: './home.component.html',
