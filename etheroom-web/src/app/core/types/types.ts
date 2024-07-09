0adbef66801137e6d508b54d981d2b25c16a96da
Robert-Martins <robertcantares@gmail.com>
2024-07-09 20:57:05 -0300

    feat/define-types

---
+++ etheroom-web/src/app/core/types/types.ts
@@ -0,0 +1,52 @@
+export type Enum = {
+    name: string;
+    description: string;
+}
+
+export type ApplicationError = {
+    error: any | EtheroomApplicationError;
+}
+
+export type EtheroomApplicationError = {
+    title: string;
+    status: number;
+    details: string;
+    developerMessage: string;
+    className: string;
+    timestamp: Date;
+}
+
+export type AuthenticationRequest = {
+    ethereumAddress: string;
+    ethereumPublicKey: string;
+}
+
+export type AuthenticationResponse = {
+    accessToken: string;
+}
+
+export type Token = {
+    token: string;
+    hash: string;
+    type: string;
+}
+
+export type JwtTokenClaims = {
+    sub: string;
+    role: string;
+    iat: number;
+    exp: number;
+    id: string;
+}
+
+export type Page<T> = {
+    content: T[];
+    totalElements: number;
+    totalPages: number;
+    size: number;
+    number: number;
+    numberOfElements: number;
+    first: boolean;
+    last: boolean;
+    empty: boolean;
+}
