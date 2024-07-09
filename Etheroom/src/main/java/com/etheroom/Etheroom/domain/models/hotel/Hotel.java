62920686f4189467cb7665762e910f29d155a6eb
Robert-Martins <robertcantares@gmail.com>
2024-07-09 18:58:31 -0300

    feat/enhance-hotel-entity

--- Etheroom/src/main/java/com/etheroom/Etheroom/domain/models/hotel/Hotel.java
+++ Etheroom/src/main/java/com/etheroom/Etheroom/domain/models/hotel/Hotel.java
@@ -2,6 +2,9 @@ package com.etheroom.Etheroom.domain.models.hotel;
 
 import com.etheroom.Etheroom.domain.models.address.Address;
 import com.etheroom.Etheroom.domain.models.contact.Contact;
+import com.etheroom.Etheroom.domain.models.hotel.aggregates.Convenience;
+import com.etheroom.Etheroom.domain.models.hotel.aggregates.TouristSpot;
+import com.etheroom.Etheroom.domain.models.media.Media;
 import com.etheroom.Etheroom.domain.models.user.User;
 import com.etheroom.Etheroom.infrastructure.base.BaseEntity;
 import com.etheroom.Etheroom.presentation.dtos.hotel.HotelDto;
@@ -9,6 +12,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Getter
@@ -23,17 +28,45 @@ public class Hotel extends BaseEntity {
     @Column(name = "description")
     private String description;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", updatable = false)
     private User user;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "address_id", referencedColumnName = "id", updatable = false)
     private Address address;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "contact_id", referencedColumnName = "id")
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "contact_id", referencedColumnName = "id", updatable = false)
     private Contact contact;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "thumbnail_id", referencedColumnName = "id", updatable = false)
+    private Media thumbnail;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "etheroom_hotel_convenience",
+            joinColumns = @JoinColumn(name = "hotel_id"),
+            inverseJoinColumns = @JoinColumn(name = "convenience_id")
+    )
+    private List<Convenience> conveniences = new ArrayList<>();
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "etheroom_hotel_tourist_spots",
+            joinColumns = @JoinColumn(name = "hotel_id"),
+            inverseJoinColumns = @JoinColumn(name = "tourist_spot_id")
+    )
+    private List<TouristSpot> touristSpots = new ArrayList<>();
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "etheroom_hotel_images",
+            joinColumns = @JoinColumn(name = "hotel_id"),
+            inverseJoinColumns = @JoinColumn(name = "media_id")
+    )
+    private List<Media> images = new ArrayList<>();
 
     @Override
     public HotelDto mapEntityToDto() {
@@ -55,6 +88,26 @@ public class Hotel extends BaseEntity {
                 Optional.ofNullable(this.getContact())
                         .map(Contact::mapEntityToDto)
                         .orElse(null)
+        );
+        hotelDto.setThumbnail(
+                Optional.ofNullable(this.getThumbnail())
+                        .map(Media::mapEntityToDto)
+                        .orElse(null)
+        );
+        hotelDto.setConveniences(
+                this.getConveniences().stream()
+                        .map(Convenience::mapEntityToDto)
+                        .toList()
+        );
+        hotelDto.setImages(
+                this.getImages().stream()
+                        .map(Media::mapEntityToDto)
+                        .toList()
+        );
+        hotelDto.setTouristSpots(
+                this.getTouristSpots().stream()
+                        .map(TouristSpot::mapEntityToDto)
+                        .toList()
         );
         hotelDto.setUpdatedAt(this.getUpdatedAt());
         hotelDto.setCreatedAt(this.getCreatedAt());
--- Etheroom/src/main/java/com/etheroom/Etheroom/presentation/dtos/hotel/HotelDto.java
+++ Etheroom/src/main/java/com/etheroom/Etheroom/presentation/dtos/hotel/HotelDto.java
@@ -4,10 +4,15 @@ import com.etheroom.Etheroom.domain.models.hotel.Hotel;
 import com.etheroom.Etheroom.infrastructure.base.BaseEntityDto;
 import com.etheroom.Etheroom.presentation.dtos.address.AddressDto;
 import com.etheroom.Etheroom.presentation.dtos.contact.ContactDto;
+import com.etheroom.Etheroom.presentation.dtos.hotel.aggregates.ConvenienceDto;
+import com.etheroom.Etheroom.presentation.dtos.hotel.aggregates.TouristSpotDto;
+import com.etheroom.Etheroom.presentation.dtos.media.MediaDto;
 import com.etheroom.Etheroom.presentation.dtos.user.UserDto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Getter
@@ -19,6 +24,10 @@ public class HotelDto extends BaseEntityDto<Hotel> {
     private UserDto user;
     private AddressDto address;
     private ContactDto contact;
+    private MediaDto thumbnail;
+    private List<ConvenienceDto> conveniences = new ArrayList<>();
+    private List<MediaDto> images = new ArrayList<>();
+    private List<TouristSpotDto> touristSpots = new ArrayList<>();
 
     @Override
     public Hotel mapDtoToEntity() {
@@ -40,6 +49,28 @@ public class HotelDto extends BaseEntityDto<Hotel> {
                 Optional.ofNullable(this.getContact())
                         .map(ContactDto::mapDtoToEntity)
                         .orElse(null)
+        );
+        hotel.setThumbnail(
+                Optional.ofNullable(this.getThumbnail())
+                        .map(MediaDto::mapDtoToEntity)
+                        .orElse(null)
+        );
+        hotel.setConveniences(
+                this.getConveniences()
+                        .stream()
+                        .map(ConvenienceDto::mapDtoToEntity)
+                        .toList()
+        );
+        hotel.setImages(
+                this.getImages().stream()
+                        .map(MediaDto::mapDtoToEntity)
+                        .toList()
+        );
+        hotel.setTouristSpots(
+                this.getTouristSpots()
+                        .stream()
+                        .map(TouristSpotDto::mapDtoToEntity)
+                        .toList()
         );
         hotel.setUpdatedAt(this.getUpdatedAt());
         hotel.setCreatedAt(this.getCreatedAt());
