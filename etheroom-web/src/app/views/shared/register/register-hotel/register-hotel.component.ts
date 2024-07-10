66ad6248dce0ff50df5db850646b9bff2886e44f
Gabriel Camilo <74691430+GabrielCamiloOliveira@users.noreply.github.com>
2024-07-10 20:46:03 -0300

    Sign Up page SCSS bugfixes, Sign Up Hotel Page prototype

---
+++ etheroom-web/src/app/views/shared/register/register-hotel/register-hotel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-hotel',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-hotel.component.html',
+  styleUrls: ['./register-hotel.component.scss', '../../../../../styles/global.scss']
+})
+export class RegisterHotelComponent {
+
+}
