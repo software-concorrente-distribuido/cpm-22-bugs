089473657b570a4877f1b2e7234a800a9cc98d56
Gabriel Camilo <74691430+GabrielCamiloOliveira@users.noreply.github.com>
2024-07-10 21:34:40 -0300

    Amenities and attractions lists, signup-hotel router

--- etheroom-web/src/app/views/shared/register/register-hotel/register-hotel.component.ts
+++ etheroom-web/src/app/views/shared/register/register-hotel/register-hotel.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-hotel',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register-hotel.component.html',
   styleUrls: ['./register-hotel.component.scss', '../../../../../styles/global.scss']
 })
@@ -11,12 +12,28 @@ export class RegisterHotelComponent {
 
   amenity: string = '';
   amenities: string[] = [];
+  attraction: string = '';
+  attractions: string[] = [];
 
-  addAmenity() {
+  public addAmenity() {
+    console.log('Current Amenity:', this.amenity); // Adicionado para depuração
     if (this.amenity.trim() !== '') {
       this.amenities.push(this.amenity.trim());
       this.amenity = '';
-      console.log('Amenities:', this.amenities); //Verificando se comodidades estão sendo adicionadas
+      console.log('Amenities:', this.amenities); // Verificando se comodidades estão sendo adicionadas
+    } else {
+      console.log('Amenity is empty or only spaces'); // Adicionado para depuração
+    }
+  }
+
+  public addAttraction() {
+    console.log('Current Attraction:', this.attraction); // Adicionado para depuração
+    if (this.attraction.trim() !== '') {
+      this.attractions.push(this.attraction.trim());
+      this.attraction = '';
+      console.log('Attractions:', this.attractions); // Verificando se comodidades estão sendo adicionadas
+    } else {
+      console.log('Attraction is empty or only spaces'); // Adicionado para depuração
     }
   }
 
