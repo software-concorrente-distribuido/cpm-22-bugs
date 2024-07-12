a5f27d8a55e8e6435c6c38c8d91e7660421cfc66
Robert-Martins <robertcantares@gmail.com>
2024-07-11 22:31:32 -0300

    feat/define-forms-module-and-initial-components

---
+++ etheroom-web/src/app/shared/components/forms/hotel-form/hotel-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hotel-form',
+  templateUrl: './hotel-form.component.html',
+  styleUrl: './hotel-form.component.scss'
+})
+export class HotelFormComponent {
+
+}
