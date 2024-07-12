385c6bf111505bdd0f8eee4c0b120cbeb2ce63e9
Robert-Martins <robertcantares@gmail.com>
2024-07-11 22:29:17 -0300

    feat/define-pipe-module-and-common-pipes

---
+++ etheroom-web/src/app/shared/pipes/dateformat.pipe.ts
@@ -0,0 +1,14 @@
+import { DatePipe } from '@angular/common';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateformat'
+})
+export class DateformatPipe implements PipeTransform {
+
+  public transform(value: any, format: string = 'dd/MM/yyyy HH:mm'): string {
+    const datePipe = new DatePipe('en-US');
+    return datePipe.transform(value, format);
+  }
+
+}
