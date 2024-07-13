4d2308b8ffa288e51423c65d6a595df264d5730d
WendelFl <wendelfl.dev@gmail.com>
2024-07-13 18:26:06 -0300

    :closed_lock_with_key: feat: updating signing message

--- etheroom-web/src/environments/environments.development.ts
+++ etheroom-web/src/environments/environments.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:9090',
-    ETHEROOM_APP_MESSAGE: 'HelloWorld'
+    ETHEROOM_APP_MESSAGE: 'Login Etheroom'
 }
--- etheroom-web/src/environments/environments.ts
+++ etheroom-web/src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'http://localhost:9090',
-    ETHEROOM_APP_MESSAGE: 'HelloWorld'
+    ETHEROOM_APP_MESSAGE: 'Login Etheroom'
 }
