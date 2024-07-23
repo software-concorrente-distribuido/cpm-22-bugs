2369792883d800cee3523050392fec7443ad20b0
Gustavo Morais <medeiros@discente.ufg.br>
2024-07-23 16:29:23 -0300

    Atualizando componentes

--- etheroom-web/src/app/shared/components/ether-table/models/ether-row-def-context.model.ts
+++
@@ -1,9 +0,0 @@
-export class EtherRowDefContext<T> {
-    public $implicit: any;
-    public index: number;
-
-    constructor($implicit: T, index: number) {
-        this.$implicit = $implicit;
-        this.index = index;
-    }
-}
