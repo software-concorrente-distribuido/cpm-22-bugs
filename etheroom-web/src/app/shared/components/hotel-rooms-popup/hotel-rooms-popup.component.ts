5030f9fc564892b00b887d45c8274c977051b95b
Gabriel Camilo <74691430+GabrielCamiloOliveira@users.noreply.github.com>
2024-07-30 16:13:50 -0300

    Hotel room popup SCSS

--- etheroom-web/src/app/shared/components/hotel-rooms-popup/hotel-rooms-popup.component.ts
+++ etheroom-web/src/app/shared/components/hotel-rooms-popup/hotel-rooms-popup.component.ts
@@ -2,19 +2,29 @@ import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
+import { EtherIconTextComponent } from "../../../shared/components/ether-icon-text/ether-icon-text.component";
+import { ButtonsModule } from "../buttons/buttons.module";
 
 @Component({
   selector: 'app-hotel-rooms-popup',
   standalone: true,
-  imports: [CommonModule, MatDialogModule, MatCardModule],
+  imports: [CommonModule, MatDialogModule, MatCardModule, EtherIconTextComponent, ButtonsModule],
   templateUrl: './hotel-rooms-popup.component.html',
   styleUrl: './hotel-rooms-popup.component.scss'
 })
 export class HotelRoomsPopupComponent {
+openRoomsDialog(arg0: any) {
+throw new Error('Method not implemented.');
+}
 
   constructor(
+    private router: Router,
     public dialogRef: MatDialogRef<HotelRoomsPopupComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
-  
+
+  goToRoomPage(id: any) {
+    this.router.navigate([`/room-page`, id]);
+  }
 }
